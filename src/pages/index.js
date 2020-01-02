81a2c8dadabb7ee55b3a3887f23faf4e95397bf5
Olivia Briggs <ofbriggs@gmail.com>
2020-01-02 13:18:37 -0500

    Style title

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,7 +90,9 @@ function Home() {
       >
         <header className={classnames("hero hero--primary", styles.heroBanner)}>
           <div className="container">
-            <h1 className="hero__title">{siteConfig.title}</h1>
+            <h1 className="hero__title">
+              V<span>I</span>A
+            </h1>
             <p className="hero__subtitle">{siteConfig.tagline}</p>
           </div>
         </header>
