6cb8f933a68f8614ff729dd993452e3bcfa77c39
ValdisThomann <valdis.thomann@gmail.com>
2020-01-10 23:35:07 +1100

    updated firmware list

--- scripts/Create-FirmwareLinks.ps1
+++ scripts/Create-FirmwareLinks.ps1
@@ -1,10 +1,11 @@
 ﻿$response = curl https://api.github.com/repos/the-via/firmware/contents
 $files = $response.Content | ConvertFrom-Json
+Remove-Item C:\temp\firmware.txt
 $files | % {
     $path = $_.path.Trim()
     $downloadUrl = $_.download_url.Trim()
     if ($path.EndsWith(".hex") -or $path.EndsWith(".bin")) {
-        "<DownloadAsFile url=""$downloadUrl"" filename=""$path""></DownloadAsFile>"
+        "<Download url=""$downloadUrl"" filename=""$path""></Download>"
     }
 } >> C:\temp\firmware.txt
 
