afa16f0764fe98cec1eeb5e4cbb7072fd81db328
ValdisThomann <valdis.thomann@gmail.com>
2020-01-23 00:23:42 +1100

    Updated firmware list

--- scripts/Create-FirmwareLinks.ps1
+++ scripts/Create-FirmwareLinks.ps1
@@ -1,11 +1,21 @@
 ﻿$response = curl https://api.github.com/repos/the-via/firmware/contents
 $files = $response.Content | ConvertFrom-Json
-Remove-Item C:\temp\firmware.txt
+
+$firmwareList = "C:\temp\firmware.txt"
+$keyboardList = "C:\temp\keyboards.txt"
+
+"" > $firmwareList
+"| Compatible as of $((Get-Date).ToString('dd MMM yyyy')) |" > $keyboardList
+"| --------------------------- |" >> $keyboardList
+
+$textInfo = (Get-Culture).TextInfo
 $files | % {
     $path = $_.path.Trim()
     if ($path.EndsWith(".hex") -or $path.EndsWith(".bin")) {
-        "<Download filename=""$path""></Download>"
+        "<Download filename=""$path""></Download>" >> $firmwareList
+        $name = $_.name -replace "_via.(hex|bin)", "" -replace "_", " "
+        "| $($textInfo.ToTitleCase($name)) |" >> $keyboardList
     }
-} >> C:\temp\firmware.txt
+} 
 
 #TODO: grab keyboard names for supported keyboards and run on build
