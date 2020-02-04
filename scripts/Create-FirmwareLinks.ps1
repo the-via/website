31e470a95bd723572aa97eb03cb1f860cc621b31
ValdisThomann <valdis.thomann@gmail.com>
2020-02-04 21:43:25 +1100

    Updated keyboards and firmware, updated script to auto-gen keyboards

--- scripts/Create-FirmwareLinks.ps1
+++ scripts/Create-FirmwareLinks.ps1
@@ -1,21 +1,35 @@
-﻿$response = curl https://api.github.com/repos/the-via/firmware/contents
+﻿function Get-TableHeader() {
+    return @"
+    | Compatible as of $((Get-Date).ToString('dd MMM yyyy')) |
+    | ---------------------------- |
+"@
+}
+
+### Firmware
+
+$response = curl https://api.github.com/repos/the-via/firmware/contents
 $files = $response.Content | ConvertFrom-Json
 
 $firmwareList = "C:\temp\firmware.txt"
-$keyboardList = "C:\temp\keyboards.txt"
 
 "" > $firmwareList
-"| Compatible as of $((Get-Date).ToString('dd MMM yyyy')) |" > $keyboardList
-"| --------------------------- |" >> $keyboardList
 
-$textInfo = (Get-Culture).TextInfo
+
 $files | % {
     $path = $_.path.Trim()
     if ($path.EndsWith(".hex") -or $path.EndsWith(".bin")) {
         "<Download filename=""$path""></Download>" >> $firmwareList
-        $name = $_.name -replace "_via.(hex|bin)", "" -replace "_", " "
-        "| $($textInfo.ToTitleCase($name)) |" >> $keyboardList
     }
 } 
 
-#TODO: grab keyboard names for supported keyboards and run on build
+### Keyboards
+
+$response = curl https://www.caniusevia.com/keyboards.v2.json
+$keyboards = ($response.Content | ConvertFrom-Json).definitions
+
+$keyboardList = "C:\temp\keyboards.txt"
+Get-TableHeader > $keyboardList
+
+$keyboards.PSObject.Properties | % {
+    "| $($_.value.name) |"
+} | Sort-Object >> $keyboardList
